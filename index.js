fa46b36e0a1588a30ede801e9b6ce856648d2b4c
RamazaN <ramazan.abdullaev.00@gmail.com>
2022-11-14 13:14:13 +0300

    main

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.assets__wrapper').slick({
+		dots: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		arrows: false
+	});
+ });
+	 
